3f2393b9f476efb4ba9c8ceb903652314ad227e8
rogeriocerqueira <rogeriocerqueira.uefs@gmail.com>
2021-03-17 18:08:28 -0300

    Add files via upload
    são códigos bem simples mas acredito que consegue ver meu estilo de programação. Se não ficar claro minha forma de programar eu posso colocar mais códigos ou desafio que você me propor ou eventualmente coisas que pretendo fazer na área de dados ou algo interessante que eu queira tentar me arriscar a fazer.

---
+++ analise_de_perfil.py
@@ -0,0 +1,202 @@
+import random
+import time
+#Abre o arquivo para a leitura e retorna a lista com todas as perguntas do arquivo
+def abre_arquivo(nome_arquivo):
+    arq_perguntas = []
+    try:
+        with open(nome_arquivo, 'r') as objeto_arquivo:
+            for linha in objeto_arquivo:
+                arq_perguntas.append(linha)
+            return arq_perguntas
+    except IOError:
+        print('O arquivo não foi encontrado!')
+        exit()
+
+#Limpa a tela no terminal, apenas dando um scroll para cima pois a IDE não limpa nescessáriamente
+def limpa_tela(tam = 7):
+    clear = '\n'
+    print(clear*tam)
+
+''' +----------------------------------------------------------------------------------+
+        Através de uma palavra-chave que ajuda a saber qual a pergunta do usuário, 
+        o algoritmo compara a pergunta do usuário e responde a pergunta 
+    +----------------------------------------------------------------------------------+ '''
+
+def responde_perguntas(lista, nome_arquivo,palavras_chave):
+
+    resp = 'S'
+    while resp == 'S' or resp =='s':
+        pergunta = input('Digite a sua pergunta:')
+        meu_perfil = {}
+        posicao = 0
+
+
+        ''' +-------------------------------------------------------------------------------------------+
+                O laço abaixo verifica a ocorrência de alguma palavra-chave na string pra saber qual 
+                foi a pergunta que o usuário digitou baseado na palavra-chave, caso não seja encontrada 
+                nenhuma das palavras-chave, retorna uma reposta negativa para o usuário informando que não
+                sabe responder a pergunta! 
+            +--------------------------------------------------------------------------------------------+ '''
+
+        for i in range(len(palavras_chave)):
+            chave = palavras_chave[i]
+
+            ''' +-----------------------------------------------------------------------------------------+
+                        Se a substtring é igual a chave então, pode ser que pergunta é uma pergunta 
+                        cadatrada no arquivo (pergunta.txt) ou seja, existe um conjunto de resposta
+                        possíveis para ela ser respondida.
+                +------------------------------------------------------------------------------------------+ '''
+
+            for j in range(len(pergunta)):
+                if pergunta[j:len(chave)+j] == chave:
+                    pergunta = lista[i]
+                    pergunta = pergunta[:-1]
+                    resposta = seleciona_resposta(i+1, nome_arquivo = 'respostas')
+                    meu_perfil[pergunta] = resposta
+                    print(meu_perfil)
+                    time.sleep(3)
+                    limpa_tela()
+                    break
+             
+        if not meu_perfil:
+            print('Desculpa essa pergunta, ainda não sabemos responder!')
+            time.sleep(3)
+            limpa_tela()
+                    
+
+        resp = input('Deseja fazer outra pergunta? S/N:')
+        limpa_tela()
+
+        if resp == 'N' or resp == 'n':
+            main()
+
+        ''' +----------------------------------------------------------------------------------------+
+                Agora eu preciso colocar perguntas e respostas numa estrutura de dados pra relacionar
+                esses dados. Eu escolhi a Estrutura de Dados dicionário por ser, na minha opinião o
+                melhor pra visulizar nessa situação. O próximo passo agora é só relacionar perguntas
+                e resposta numa mesma estrutura em arquivos diferentes. Pra isso eu preciso chamar outra
+                função que eu criei que vai, ler o arquivo de resposta, pegar uma resposta com base na
+                pergunta feita e devolver uma resposta aleartória, que será sorteada a cada chamada da função.
+             +---------------------------------------------------------------------------------------+ '''
+
+def seleciona_resposta(i, nome_arquivo):
+
+        ''' +-------------------------------------------------------------------------------
+                Aqui eu tenho que ler o arquivo resposta e com base na posição em que está a
+                a pergunta eu sei exatamente qual a string do arquivo repostas eu vou sortear.
+                Aqui tenho que garantir 2 coisas, muito importante:
+
+                1°: Que não haja saltos de linhas duplos (ENTER) entre os campos que separam cada
+                possível conjunto de repostas, nem mesmo no final do arquivo por que isso vai me permitir que
+                linhas em branco serão usadas como identificador de posição do conjunto de respostas que eu devo
+                guardar na estrutura (tupla, lista, etc), nesse caso.
+
+                2°: Que o arquivo esteja ordenado. Eu preciso garantir que os arquivo esteja ordenado,
+                    ou seja, cada espaço em branco no arquivo significa um conjunto de resposta para
+                    a posssível pergunta feita pelo usuário.
+
+            +-------------------------------------------------------------------------------+'''
+        respostas = []
+        cont = 0
+        with open(nome_arquivo, 'r') as arquivo:
+            for linha in arquivo:
+               linha = linha.strip()
+
+               if len(linha) == False:
+                    cont+=1
+
+               elif cont == i and len(linha)>1:
+                    respostas.append(linha)
+        p = random.choice(respostas)
+        return p
+
+def main():
+    print('-------------------------------')
+    print('Menu:')
+    print('-------------------------------')
+    resp = int(input('0 - Introdução \n1 - Fazer Perguntas \n2 - Inserir Perguntas \n3 - Sair \n'))
+    limpa_tela()
+
+    if resp ==1:
+        nome_arquivo = input('Digite o nome do arquivo:')
+        lista = abre_arquivo(nome_arquivo)
+
+        with open('palavras_chaves', 'r+') as arquivo:
+            palavra_chave = []
+            for linha in arquivo:
+                linha = linha.strip()
+                palavra_chave.append(linha)
+        responde_perguntas(lista, nome_arquivo, palavra_chave)
+
+    elif resp == 2:
+
+        with open('perguntas', 'a') as arquivo:
+            pergunta = input('Digite sua pergunta:')
+            if pergunta == '':
+                print('Pergunta inválida ou vazia!')
+                print('Tente novamente mais tarde!')
+                exit()
+            else:
+                linha = arquivo.write('\n')
+                linha = arquivo.write(pergunta)
+
+
+        with open('respostas', 'a') as arquivo2:
+            resp = 'S'
+            i = 0
+            while resp == 's' or resp == 'S':
+                respostas = input('Digite a %d° resposta' %(i+1))
+                if i == 0 and respostas != '':
+
+                    linha = arquivo2.write('\n')
+                    linha = arquivo2.write('\n')
+                    linha = arquivo2.write(respostas)
+                    i += 1
+                elif i > 0 and respostas != '':
+
+                    linha = arquivo2.write('\n')
+                    linha = arquivo2.write(respostas)
+                    i += 1
+
+                else:
+                    print('Respostas inválida ou vázia!')
+
+                resp = input('Deseja continuar S/N?')
+
+        print('Respostas cadastradas com sucesso!')
+
+        with open('palavras_chaves', 'r+') as arquivo:
+            aux =  input('Digite uma palavra chave que tenha haver com a pergunta:')
+
+            if aux != '':
+                linha = arquivo.write('\n')
+                linha = arquivo.write(aux)
+            else:
+                 linha = arquivo.write('\n')
+                 linha = arquivo.write('no key word')
+
+            palavra_chave = []
+            for linha in arquivo:
+                linha = linha.strip()
+                palavra_chave.append(linha)
+
+    elif resp ==3:
+        exit()
+
+    elif resp == 0:
+        with open('welcome', 'r') as arquivo:
+
+            for linha in arquivo:
+                linha = linha.strip()
+                print(linha)
+        print('\n')
+        resp = input('Deseja retornar pra o menu anterior S/N ?')
+        limpa_tela()
+        if resp == 's' or resp =='S':
+            if resp =='s' or resp == 's':
+                main()
+    else:
+        print('Opção inválida!')
+
+if __name__ == '__main__':
+    main()
